e52eb3294cb64427b896af0229da0c6f72e05e2d
Omar Rehan <omarrehan7777777@gmail.com>
2025-05-08 01:27:08 +0300

    5

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/simple-weather-app", // ← استبدل باسم الريبو بالضبط
 	plugins: [react()],
+	base: "/simple-weather-app/",
 });
